c1680b66b4b9e64ac99fdd3ebea572c5b53ee3ab
RafaelMDM <rafamdmarques@gmail.com>
2019-09-14 14:44:39 -0300

    Including old files

---
+++ controller.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+	
+var questionNumber=0;
+var questionBank=new Array();
+var stage="#game1";
+var stage2=new Object;
+var questionLock=false;
+var numberOfQuestions;
+var score=0;
+		 
+		 
+ 		$.getJSON('perguntas.json', function(data) {
+
+		for(i=0;i<data.quizlist.length;i++){ 
+			questionBank[i]=new Array;
+			questionBank[i][0]=data.quizlist[i].question;
+			questionBank[i][5]=data.quizlist[i].questionTxt;
+			questionBank[i][1]=data.quizlist[i].option1;
+			questionBank[i][2]=data.quizlist[i].option2;	
+			questionBank[i][3]=data.quizlist[i].option3;
+			questionBank[i][4]=data.quizlist[i].option4;
+		}
+		 numberOfQuestions=questionBank.length; 
+				 
+		displayQuestion();
+		})// Lê os dados do arquivo .JSON
+ 
+
+function displayQuestion(){
+ var rnd=Math.random()*4;
+     rnd=Math.ceil(rnd);
+ var q1;
+ var q2;
+ var q3;
+ var q4;
+
+if(rnd==1){q1=questionBank[questionNumber][1];q2=questionBank[questionNumber][2];q3=questionBank[questionNumber][3];q4=questionBank[questionNumber][4];}
+if(rnd==2){q2=questionBank[questionNumber][1];q3=questionBank[questionNumber][2];q4=questionBank[questionNumber][3];q1=questionBank[questionNumber][4];}
+if(rnd==3){q3=questionBank[questionNumber][1];q4=questionBank[questionNumber][2];q1=questionBank[questionNumber][3];q2=questionBank[questionNumber][4];}
+if(rnd==4){q4=questionBank[questionNumber][1];q1=questionBank[questionNumber][2];q2=questionBank[questionNumber][3];q3=questionBank[questionNumber][4];}
+
+$(stage).append('<div class="questionImage"><img src="img/'+questionBank[questionNumber][0]+'"</div><div class="questionText">'+questionBank[questionNumber][5]+'</div><div id="1" class="option">'+q1+'</div><div id="2" class="option">'+q2+'</div><div id="3" class="option">'+q3+'</div><div id="4" class="option">'+q4+'</div>');
+
+ $('.option').click(function(){
+  if(questionLock==false){questionLock=true;	
+  //Resposta correta
+  if(this.id==rnd){
+   $(stage).append('<div class="feedback1">CORRETO</div>');
+   score++;
+   }
+  //Resposta errada	
+  if(this.id!=rnd){
+   $(stage).append('<div class="feedback2">ERRADO</div>');
+  }
+  setTimeout(function(){changeQuestion()},1000);
+ }})
+}
+	
+	function changeQuestion(){
+		
+		questionNumber++;
+	
+	if(stage=="#game1"){stage2="#game1";stage="#game2";}
+		else{stage2="#game2";stage="#game1";}
+	
+	if(questionNumber<numberOfQuestions){displayQuestion();}
+	else{displayFinalSlide();}
+	
+	 $(stage2).animate({"right": "+=800px"},"slow", function() {$(stage2).css('right','-800px');$(stage2).empty();});
+	 $(stage).animate({"right": "+=800px"},"slow", function() {questionLock=false;});
+	}//Muda p/ a próxima pergunta
+		
+	function displayFinalSlide(){
+		
+		$(stage).append('<div class="finalSlide">Você completou o Quiz!<br><br>Total de perguntas: '+numberOfQuestions+'<br>Respostas corretas: '+score+'</div>');
+		
+	}//Mostra o último slide
+});
